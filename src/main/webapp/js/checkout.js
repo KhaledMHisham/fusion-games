ffa67e9ae0a0088679229bd5070ac98c187b663a
KhaledMHisham <39314719+KhaledMHisham@users.noreply.github.com>
2023-03-29 00:58:41 +0200

    Update checkout.js

--- src/main/webapp/js/checkout.js
+++ src/main/webapp/js/checkout.js
@@ -68,7 +68,8 @@ function validatePhone() {
 }
 
 function validateCreditNum() {
-  const regex = /^(?:4[0-9]{12}(?:[0-9]{3})?|5[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11})$/;
+  const regex = /^([0-9]{4}-){3}[0-9]{4}$/
+
   const cardNumValue = cardNumInput.value;
   if (regex.test(cardNumValue)) {
     cardNumError.innerHTML = "";
